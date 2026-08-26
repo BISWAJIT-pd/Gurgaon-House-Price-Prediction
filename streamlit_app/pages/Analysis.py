3d814ee1d9893b05d043994a26d3558429426070
BISWAJIT GIRI <biswajit83919@gmail.com>
2026-08-27 00:57:49 +0530

    Update Analysis.py

--- streamlit_app/pages/Analysis.py
+++ streamlit_app/pages/Analysis.py
@@ -6,7 +6,10 @@ from wordcloud import WordCloud
 import matplotlib.pyplot as plt
 import plotly.express as px
 import joblib
-new_df=pd.read_csv('dataset/data_viz1.csv')
+from pathlib import Path
+
+BASE_DIR = Path(__file__).resolve().parent.parent
+new_df = pd.read_csv(BASE_DIR / 'dataset' / 'data_viz1.csv')
 feature_text = joblib.load("dataset/feature_text.pkl")
 group_df = new_df.groupby('sector')[
     ['price','price_per_sqft','built_up_area','latitude','longitude']
